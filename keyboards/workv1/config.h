78b7b8005e4af67a68b7ef59d5744debd65bd01d
jkcloutier330 <jkcloutier330@gmail.com>
2022-09-16 18:08:44 -0400

    duh

--- keyboards/workv1/config.h
+++ keyboards/workv1/config.h
@@ -3,18 +3,47 @@
 
 #pragma once
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
+#ifndef CONFIG_H
+#define CONFIG_H
 
-/* disable debug print */
-//#define NO_DEBUG
+#include "config_common.h"
 
-/* disable print */
-//#define NO_PRINT
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x6060
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    qmkbuilder
+#define PRODUCT         keyboard
+#define DESCRIPTION     Keyboard
 
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+/* key matrix size */
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 20
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS { PB4, PB5, PB6, PB7, PB8, PB9 }
+#define MATRIX_COL_PINS { PA7, PA6, PA5, PA4, PA3, PA2, PA11, PA12, PA15, PB3 }
+#define UNUSED_PINS
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCING_DELAY 5
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* prevent stuck modifiers */
+#define PREVENT_STUCK_MODIFIERS
+
+/* RGB crap */
+#define RGB_DI_PIN PB2
+#define DRIVER_LED_TOTAL 5
+
+/* LED Indicators */
+#define LED_NUM_LOCK_PIN PB15
+#define LED_CAPS_LOCK_PIN PB10
+
+
+#endif
