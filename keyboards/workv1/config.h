be93ea2c92ff3cab060fdfde546f60e1c3c23987
jkcloutier330 <jkcloutier330@gmail.com>
2022-09-17 11:23:37 -0400

    ahhh

--- keyboards/workv1/config.h
+++ keyboards/workv1/config.h
@@ -14,14 +14,14 @@
 
 /* key matrix size */
 #define MATRIX_ROWS 6
-#define MATRIX_COLS 20
+#define MATRIX_COLS 10
 
 /* key matrix pins */
-#define MATRIX_ROW_PINS { PB4, PB5, PB6, PB7, PB8, PB9 }
-#define MATRIX_COL_PINS { PA7, PA6, PA5, PA4, PA3, PA2, PA11, PA12, PA15, PB3 }
+#define MATRIX_ROW_PINS { A10, B5, B6, B7, B8, B9 }
+#define MATRIX_COL_PINS { B1, B0, A7, A6, A5, A4, B12, B13, B14, B15 }
 
 /* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION CUSTOM_MATRIX
+//#define DIODE_DIRECTION CUSTOM_MATRIX
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
@@ -30,10 +30,12 @@
 #define LOCKING_RESYNC_ENABLE
 
 /* RGB crap */
-#define RGB_DI_PIN PB2
-#define DRIVER_LED_TOTAL 5
+#define RGB_DI_PIN B10
+#define RGBLED_NUM 7
 
-/* LED Indicators */
-#define LED_NUM_LOCK_PIN PB15
-#define LED_CAPS_LOCK_PIN PB10
+#define RGBLIGHT_LAYERS
+
+#define RGBLIGHT_LIMIT_VAL 200 /* The maximum brightness level */
+#define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+
 
--- platforms/wait.h
+++ platforms/wait.h
@@ -21,9 +21,9 @@
 extern "C" {
 #endif
 
-#if __has_include_next("_wait.h")
-#    include_next "_wait.h" /* Include the platforms _wait.h */
-#endif
+//#if __has_include_next("_wait.h")
+//#    include_next "_wait.h" /* Include the platforms _wait.h */
+//#endif
 
 #ifdef __cplusplus
 }
